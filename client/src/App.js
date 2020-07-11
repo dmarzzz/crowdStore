ae5e1608568351516a98a1ad02622a437be6b72b
dmarzzz <danmarzec@icloud.com>
2020-07-11 16:14:09 -0400

    display metamask eth addr

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './App.css';
+// import { ethers } from "ethers";
+
+// //connect to in memory blockchain
+// const localProvider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+
+// //connect to in mem blockchain thru metamask
+// const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+//ethers/web3 agnostic implementation
+const ethereum = window.ethereum; 
+let provider;
 
 function App() {
+
+
+  React.useEffect(() => {
+    const windowProvider = async() => {
+      if (typeof window.ethereum !== 'undefined'
+          || (typeof window.web3 !== 'undefined')) {
+
+          provider = window['ethereum'] || window.web3.currentProvider;
+
+          try{
+             await provider.enable();
+          }catch (e){
+              console.error('user refused to connect');
+          }
+          //console.log('network:', provider.networkVersion);
+          //console.log('selectedAddress:', provider.selectedAddress);
+          //console.log('is metamask:', provider.isMetaMask);
+        }
+      }
+      windowProvider();
+  });
+
+
+  const [addr, setAddr] = useState()
+  //const [injectedProvider, setInjectedProvider] = useState();
+
+  if(ethereum){
+    ethereum.on('accountsChanged', function(accounts){
+      setAddr(accounts[0])
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +52,18 @@ function App() {
           crowdStore prototype frontend 
         </p>
 
+        <p>
+          Ethereum Address: {addr}
+        </p>
+
         <label >
           Upload File: 
-      </label>
-
+        </label>
         <input
         accept="image/*"
         multiple
         type="file"
       />
-
-
       </header>
     </div>
   );
