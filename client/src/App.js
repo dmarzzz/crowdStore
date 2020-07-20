168a15fe33a3612a020f8d617460321988b44b86
dmarzzz <danmarzec@icloud.com>
2020-07-19 21:23:00 -0400

    new front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
-import { 
-  CreateToken, 
-  FilecoinBalancesList , 
-  CreateFilecoinAddress ,
-  SendAddressFilecoin , 
-  ButtonPrimary ,
-  CreateFilecoinStorageDeal } from 'slate-react-system';
+import {
+  CreateToken,
+  FilecoinBalancesList,
+  CreateFilecoinAddress,
+  SendAddressFilecoin,
+  ButtonPrimary,
+  CreateFilecoinStorageDeal
+} from 'slate-react-system';
 import { createPow } from "@textile/powergate-client";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
 
 // import { ethers } from "ethers";
 
@@ -16,7 +29,28 @@ import { createPow } from "@textile/powergate-client";
 
 // //connect to in mem blockchain thru metamask
 // const provider = new ethers.providers.Web3Provider(window.ethereum)
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  storeCard: {
+    position: 'absolute',
+    minWidth: 500,
+    minHeight: 350,
+    left: '1.5%',
+    top: '10%',
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
 //ethers/web3 agnostic implementation
 const ethereum = window.ethereum;
 let provider;
@@ -27,6 +61,11 @@ function App() {
   const [addr, setAddr] = useState()
   const [token, setToken] = useState()
   const [info, setInfo] = useState()
+  const classes = useStyles()
+  const [createDocStore, setCreateDocStore] = useState(false)
+  const [activeStep, setActiveStep] = React.useState(0)
+  const [skipped, setSkipped] = React.useState(new Set())
+  const steps = getSteps()
 
   React.useEffect(() => {
     const windowProvider = async () => {
@@ -47,12 +86,12 @@ function App() {
     }
     const powerInitialize = async () => {
 
-      try{
+      try {
         FFS = await PowerGate.ffs.create();
         console.log("powergate stuff")
-        console.log("FFS : ",  FFS);
+        console.log("FFS : ", FFS);
       }
-      catch(e){
+      catch (e) {
         console.error("couldnt connect to powergate");
       }
 
@@ -65,7 +104,7 @@ function App() {
   async function handleCreateToken() {
     // NOTE
     // Requires PowerGate to be running locally.
-    
+
     const token = FFS.token ? FFS.token : null;
     PowerGate.setToken(token);
     setToken(token);
@@ -76,13 +115,13 @@ function App() {
   }
 
   async function handleCreateAddress({ name, type, makeDefault }) {
-    
+
     const token = FFS.token ? FFS.token : null;
     PowerGate.setToken(token);
     setToken(token);
     const response = await PowerGate.ffs.newAddr(
-      name, 
-      type, 
+      name,
+      type,
       makeDefault
     );
     console.log(response);
@@ -90,22 +129,22 @@ function App() {
     setInfo(info);
   }
 
-  async function handleSend({ source, target, amount }){
+  async function handleSend({ source, target, amount }) {
     console.log("Did this fire?")
     try {
       const response = await PowerGate.ffs.sendFil(
-        source, 
-        target, 
+        source,
+        target,
         amount
       );
-      console.log("response be " , response)
+      console.log("response be ", response)
     } catch (error) {
       console.error("Erroring sending coin", error)
     }
   }
 
-  async function refresh(){
-    const {info} = await PowerGate.ffs.info();
+  async function refresh() {
+    const { info } = await PowerGate.ffs.info();
     setInfo(info);
   }
 
@@ -118,14 +157,14 @@ function App() {
   }
 
 
-  async function handleSubmit(data){
+  async function handleSubmit(data) {
     const file = data.file.files[0];
     var buffer = [];
     // NOTE(jim): A little hacky...
     const getByteArray = async () =>
       new Promise((resolve) => {
         const reader = new FileReader();
-        reader.onloadend = function(e) {
+        reader.onloadend = function (e) {
           if (e.target.readyState == FileReader.DONE) {
             buffer = new Uint8Array(e.target.result);
           }
@@ -142,67 +181,205 @@ function App() {
       console.log(job);
     }, jobId);
   }
+  function getSteps() {
+    return ['Generate new Powergate Token', 'Create Filecoin Address', 'Optional: Send Filecoin', 'Make Storage Deal'];
+  }
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return <div>
+          'Generate new Powergate Token'
+                <CreateToken
+            token={token}
+            onClick={handleCreateToken} />
+        </div>;
+      case 1:
+        return <div>
+          'Create Filecoin Address'
+        <CreateFilecoinAddress onSubmit={handleCreateAddress} />
+        </div>;
+      case 2:
+        return <div>
+          'Optional: Send Filecoin'
+        <SendAddressFilecoin onSubmit={handleSend} />
+        </div>;
+      case 3:
+        return <div>
+          'Make Storage Deal'
+        <CreateFilecoinStorageDeal onSubmit={handleSubmit} />
+        </div>;
+      default:
+        return 'Unknown step';
+    }
+  }
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          crowdStore prototype frontend
-        </p>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            CrowdStore 0.0.1
+          </Typography>
+          <Button onClick={() => { setCreateDocStore(true); console.log("docstore : ", createDocStore) }} color="inherit" >Create Doc Store</Button>
+        </Toolbar>
+      </AppBar>
 
-        <p>
-          Ethereum Address: {addr}
-        </p>
 
-        <label >
-          Upload File:
-        </label>
-        <input
-          accept="image/*"
-          multiple
-          type="file"
-        />
+      {!createDocStore ?
 
-        <CreateToken
-          token={token}
-          onClick={handleCreateToken} />
-        <div style={{ 'color': '#000000' }}>
-          {info ? <FilecoinBalancesList data={info.balancesList} /> : null}
-        <ButtonPrimary onClick={refresh}> Refresh </ButtonPrimary>
-        <CreateFilecoinAddress onSubmit={handleCreateAddress}/>
-        <SendAddressFilecoin onSubmit={handleSend} />
-        <CreateFilecoinStorageDeal onSubmit={handleSubmit} />
-        
+        <Card className={classes.storeCard}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Example CrowdStore 1
+        </Typography>
+            <Typography variant="h6">
+              eth address : {addr}
+            </Typography>
+            <Typography variant="h6">
+              # of documents : 1
+        </Typography>
+          </CardContent>
+
+        </Card>
+
+        :
+
+        <div className={classes.root}>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+              if (isStepOptional(index)) {
+                labelProps.optional = <Typography variant="caption">Optional</Typography>;
+              }
+              if (isStepSkipped(index)) {
+                stepProps.completed = false;
+              }
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          <div>
+            {activeStep === steps.length ? (
+              <div>
+                <Typography className={classes.instructions}>
+                  All steps completed - you&apos;re finished
+            </Typography>
+                <Button onClick={handleReset} className={classes.button}>
+                  Reset
+            </Button>
+              </div>
+            ) : (
+                <div>
+                  <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                  <div>
+                    <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+                      Back
+              </Button>
+                    {isStepOptional(activeStep) && (
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSkip}
+                        className={classes.button}
+                      >
+                        Skip
+                </Button>
+                    )}
+
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                  </div>
+                </div>
+              )}
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1>Address List</h1>
+          <header className="App-header">
+            <div style={{ 'color': '#000000', 'alignContent': 'center' }}>
+              {info ? <FilecoinBalancesList data={info.balancesList} /> : null}
+              <ButtonPrimary onClick={refresh}> Refresh </ButtonPrimary>
+            </div>
+          </header>
         </div>
-      </header>
-
+      }
     </div>
   );
 }
 
 export default App;
 
+{/* <p>
+crowdStore prototype frontend
+</p>
 
+<p>
+Ethereum Address: {addr}
+</p>
 
-
-// class Example extends React.Component {
-//   state = {
-//     token: null
-//   }
-//   _handleCreateToken = () => {
-//     // NOTE
-//     // Requires PowerGate to be running locally.
-//     const PowerGate = createPow({ host: 'http://0.0.0.0:6002' });
-//     const FFS = await PowerGate.ffs.create();
-//     const token = FFS.token ? FFS.token : null;
-//     PowerGate.setToken(token);
-//   }
-//   render() {
-//     return (
-//       <CreateToken 
-//         token={this.state.token} 
-//         onClick={this._handleCreateToken} />
-//     );
-//   }
-// }
+<label >
+Upload File:
+</label>
+<input
+accept="image/*"
+multiple
+type="file"
+/> */}
