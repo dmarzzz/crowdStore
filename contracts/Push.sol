70ef564e793a89377a7805109f4222b8adc637ee
gtsarfang <54679561+gtsarfang@users.noreply.github.com>
2020-08-03 10:40:45 -0400

    Update Push.sol
    Add ownership and a counting mechanism for the token id, the mint function now also returns the the token id.

--- contracts/Push.sol
+++ contracts/Push.sol
@@ -2,16 +2,27 @@
 pragma solidity ^0.6.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/drafts/Counters.sol";
 
 
 
-contract Push is ERC721Full {
+
+contract Push is Ownable, ERC721Full {
+     using Counters for Counters.Counter;
+     Counters.Counter private _id;
+
  constructor() ERC721Full("Push", "PSH") public {
-  
-function mint(address to, uint256 projectid)) public {
+ }
+    function mint(address to) public onlyOwner{
+         _id.increment();
 
-    _mint(to, projectid);
-        }
+
+         uint256 tId = _id.current();
+         _mint(to, tId);
+
+        return tId;
     }
+
 
 }
